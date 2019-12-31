8c3acbb7137254910d62e1d96fd2196835f0a004
Ashutosh Soni <viknat1994@gmail.com>
2019-12-31 18:07:49 +0530

    Added card list component structure & use of props.children
    * changed project structure.
    * Use of Porps children

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+
+import {CardList} from "./components/card-list/card-list.component";
+
 import './App.css';
 
 class App extends Component {
@@ -19,12 +22,15 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+        <CardList name="Ashu"> 
         {
           this.state.monsters.map(monster => (
             <h1 key={monster.id}>{monster.name}</h1>
           ))
 
         }
+        </CardList> 
+        
       </div>
     );
   }
