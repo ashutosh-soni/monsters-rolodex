2802220c30f657eb38857093a8e6c9cbc2991e9f
Ashutosh Soni <viknat1994@gmail.com>
2019-12-31 15:59:39 +0530

    Simple Dynamic content and key usage.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
-    this.state = {string: "Hello Ashutosh Soni"}
+    this.state = {monsters:[
+      {
+        name: "Frankenstein",
+        id:"asc1"
+      },
+      {
+        name: "Dracula",
+        id:"asc2"
+      },
+      {
+        name: "Zombie",
+        id:"asc3"
+      }
+    ]
+      
+    };
   }
 
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p> {this.state.string}</p>
-          <button onClick={()=> this.setState({string: "Hello vasu"})}> Change text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+
+        }
       </div>
     );
   }
