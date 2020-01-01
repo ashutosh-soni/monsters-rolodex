d50d4f4c0a7255b03aeae15dfa5bfdbd7fbae924
Ashutosh Soni <viknat1994@gmail.com>
2020-01-01 14:26:08 +0530

    Updates

---
+++ precache-manifest.c6e74eaac47272d8db4e9ec6a5e06ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235bf4ee4902ae01b224c2966c0d91b6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "64d762410c5a6e8ab684",
+    "url": "/monsters-rolodex/static/css/main.c6e1d867.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64d762410c5a6e8ab684",
+    "url": "/monsters-rolodex/static/js/main.174cc5ac.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
